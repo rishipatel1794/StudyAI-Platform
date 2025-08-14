a08db2a5b990209aa510c789f7703d1a6187b2a0
Rishi <rishipatel1794@gmail.com>
2025-08-14 15:09:19 +0530

    Database Connection Done

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,11 +1,16 @@
 import mongoose from 'mongoose';
+import { DB_NAME } from '../constants.js';
 
-function connectToDB() {
-    mongoose.connect(process.env.DB_CONNECT).then(() => {
-        console.log('Connected to DB');
-    }).catch((err) => {
-        console.log(err);
-    });
+const connectDB = async () => {
+    try{
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`MongoDB connected: ${connectionInstance.connection.host}`);
+        
+
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+        process.exit(1); // Exit the process with failure
+    }
 }
 
-module.exports = connectToDB;
+export default connectDB;
