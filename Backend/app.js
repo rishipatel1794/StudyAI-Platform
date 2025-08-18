1c21f85d530f9238d71e5c4b19853b8fb64a31fa
Rishi <rishipatel1794@gmail.com>
2025-08-18 20:43:47 +0530

    Some middlewares added

--- Backend/app.js
+++ Backend/app.js
@@ -1,21 +1,16 @@
-import dotenv from 'dotenv';
-import express from 'express';
+import express, { urlencoded } from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
-dotenv.config();
 const app = express();
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
 
-app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(express.static('public'));
 
-app.get('/', (req, res) => {    
-    res.send('Hello World');
-});
-
-// app.use('/users', userRoutes);
-// app.use('/captains', captainRoutes);
-
-module.exports = app;
+export { app };
--- Backend/index.js
+++ Backend/index.js
@@ -3,4 +3,12 @@ import dotenv from "dotenv";
 
 dotenv.config({path: "./.env"});
 
-connectDB();
+connectDB()
+.then(() => {
+	app.listen(process.env.PORT || 3000,()=>{
+		console.log(`Server is running on port ${process.env.PORT || 3000}`);
+	})
+})
+.catch((error) => {
+	console.log("MONGODB CONNECTION FAILED ", error);
+});
